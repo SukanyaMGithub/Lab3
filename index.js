da4dc2c6a9fcce641cc217ba8a71c9033a983aae
SukanyaMGithub <smarati@hawk.iit.edu>
2023-10-14 23:49:06 -0500

    change logic in index.js

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ bill_t.addEventListener("input", calculateTip);
 slider.addEventListener("input", calculateTip);
 
 function calculateTip() {
-    if (isNaN(bill_t.value)) {
+    let bill = bill_t.value;
+    let tip = slider.value;
+
+    // Check if the bill is not a valid number
+    if (isNaN(bill)) {
+        bill_t.value = ""; // Clear the input field
+        tip_percentage.value = "";
+        tip_amount.value = "";
+        total_amount.value = "";
         alert("ENTER A VALID INPUT");
         return; // Exit the function if the input is not a valid number
     }
 
-    let bill = parseFloat(bill_t.value);
-    let tip = parseFloat(slider.value);
+    bill = parseFloat(bill); // Convert to a number
+    tip = parseFloat(tip);
 
     tip_percentage.value = tip;
-
     let total_tip = parseFloat(((tip * bill) / 100).toFixed(2));
-
     tip_amount.value = total_tip;
-
     total_amount.value = parseFloat(bill + total_tip).toFixed(2);
 }
